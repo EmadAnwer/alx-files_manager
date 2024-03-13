515bb4219da537972e3b45d66cf16829e8fb2fc7
EmadAnwer <emadanwer.official@gmail.com>
2024-03-14 00:52:20 +0200

    [task 4]

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,5 @@
 import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
-
-import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -39,20 +37,6 @@ class AuthController {
     }
     redisClient.del(token);
     return res.status(204).send();
-  }
-
-  static async getMe(req, res) {
-    const token = req.header('X-Token');
-    const userId = await redisClient.get(`auth_${token}`);
-    if (!userId) {
-      return res.status(401).send({ error: 'Unauthorized' });
-    }
-    const collection = dbClient.client.db().collection('users');
-    const findUser = await collection.findOne({ _id: ObjectId(userId) });
-    if (!findUser) {
-      return res.status(401).send({ error: 'Unauthorized' });
-    }
-    return res.status(200).send({ id: findUser._id, email: findUser.email });
   }
 }
 

a1f80828bbee23ceef71b52407402266083ef21d
EmadAnwer <emadanwer.official@gmail.com>
2024-03-13 16:56:26 +0200

    task 3

---
+++ server.js
@@ -0,0 +1,11 @@
+import Express from 'express';
+import router from './routes';
+
+const app = new Express();
+
+app.use('/', router);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
